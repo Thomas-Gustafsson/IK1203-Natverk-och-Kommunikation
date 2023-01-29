91593fd5ed8193624baffe362bf5fc23011d93b3
Thomas <thomas.gs@outlook.com>
2023-01-29 16:25:23 +0100

    Fixed errors in this code. But it is not functional yet. I need to implement the ByteArrayOutputStream object as required per the instructions.

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -3,36 +3,34 @@ import java.net.*;
 import java.io.*;
 
 public class TCPClient {
-    private InputStream inputStream;
-    private OutputStream outputStream;
 
     public TCPClient() { // default constructor, likely to not be used in code
     }
 
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
-    Socket socket = new Socket(hostname, port);             // Creating the socket
-    Outputstream outputStream = socket.getOutputStream();   // Used to write data to server
-    Inputstream inputStream = socket.getInputStream();      //         read
+        Socket socket = new Socket(hostname, port);             // Creating the socket
+        OutputStream outputStream = socket.getOutputStream();   // Used to write data to server
+        InputStream inputStream = socket.getInputStream();      //         read
 
-    outputStream.write(toServerBytes);
-    outputstream.flush();
+        outputStream.write(toServerBytes);
+        outputStream.flush();
 
-    int totalBytesRead = 0;
-    int bytesRead;
-    byte[] fromServerBytes = new byte[1024];
-    while ((bytesRead = inputStream.read(fromServerBytes, totalBytesRead, fromServerBytes.length - totalBytesRead)) > 0) {
-        totalBytesRead += bytesRead;
-        if (totalBytesRead == fromServerBytes.length) {
-            byte[] newArray = new byte[fromServerBytes.length * 2];
-            System.arraycopy(fromServerBytes, 0, newArray, 0, fromServerBytes.length);
-            fromServerBytes = newArray;
+        int totalBytesRead = 0;
+        int bytesRead;
+        byte[] fromServerBytes = new byte[1024];
+        while ((bytesRead = inputStream.read(fromServerBytes, totalBytesRead, fromServerBytes.length - totalBytesRead)) > 0) {
+            totalBytesRead += bytesRead;
+            if (totalBytesRead == fromServerBytes.length) {
+                byte[] newArray = new byte[fromServerBytes.length * 2];
+                System.arraycopy(fromServerBytes, 0, newArray, 0, fromServerBytes.length);
+                fromServerBytes = newArray;
+            }
         }
-    }
 
-    socket.close();
+        socket.close();
 
-    byte[] response = new byte[totalBytesRead];
-    System.arraycopy(fromServerBytes, 0, response, 0, totalBytesRead);
-    return response;
+        byte[] response = new byte[totalBytesRead];
+        System.arraycopy(fromServerBytes, 0, response, 0, totalBytesRead);
+        return response;
     }
 }
